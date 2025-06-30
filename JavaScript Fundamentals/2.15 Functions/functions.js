f2ed33108a60e542950b38bbf3d89920ee0797e6
Neha-K-03 <nehak@neoito.com>
2025-06-30 22:33:57 +0530

    correction

--- JavaScript Fundamentals/2.15 Functions/functions.js
+++ JavaScript Fundamentals/2.15 Functions/functions.js
@@ -43,10 +43,10 @@ function power(x, n) {
 let x = +prompt("x?", "");
 let n = +prompt("n?", "");
 
-if(n < 1){
+if(n < 1) {
     alert(`Power ${n} is not supported, use a positive number`);
 }
 
-else{
+else {
     alert(power(x, n));
 }

f5eb237b18c9907cb5fe041142973f5b709f2bf6
Neha-K-03 <nehak@neoito.com>
2025-08-21 15:24:52 +0530

    sum the properties

---
+++ PART 1: The JavaScript Language/Data types/5.9 Object.keys, values, entries/sum_properties.js
@@ -0,0 +1,19 @@
+// TASK 1
+
+let sum = 0;
+
+function sumSalaries(salaries) {
+    for(let salary of Object.values(salaries)) {
+        sum += salary
+    }
+
+    return sum;
+}
+
+let salaries = {
+    "John": 100,
+    "Pete": 300,
+    "Mary": 250
+};
+
+alert( sumSalaries(salaries) );     // 650

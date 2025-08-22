70426e44d6892afc7da230d1abccaebe223168dc
Neha-K-03 <nehak@neoito.com>
2025-08-22 19:06:50 +0530

    minimal salary

---
+++ PART 1: The JavaScript Language/Data types/5.10 Destructuring assignment/maximal_salary.js
@@ -0,0 +1,23 @@
+// TASK 2
+
+let salaries = {
+    "John": 100,
+    "Pete": 300,
+    "Mary": 250
+};
+
+function topSalary(salaries) {
+    let maxSalary = 0;
+    let maxName = null;
+
+    for (const [name, salary] of Object.entries(salaries)) {
+        if (maxSalary < salary) {
+            maxSalary = salary;
+            maxName = name;
+       } 
+    }
+
+    return maxName;
+}
+
+alert( topSalary(salaries) );     // Pete

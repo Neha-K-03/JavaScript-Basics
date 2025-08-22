ce841a8a6cd13960be19a016de88798a1940f855
Neha-K-03 <nehak@neoito.com>
2025-08-22 23:06:42 +0530

    date creation

---
+++ PART 1: The JavaScript Language/Data types/5.11 Date and Time/date.js
@@ -0,0 +1,11 @@
+// TASK 1
+
+let d1 = new Date(2012, 1, 20, 3, 12);
+
+alert(d1);     // Mon Feb 20 2012 03:12:00
+
+// 0r
+
+let d2 = new Date("2012-02-20T03:12");
+
+alert(d2);     // Mon Feb 20 2012 03:12:00

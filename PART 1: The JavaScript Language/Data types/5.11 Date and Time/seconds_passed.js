a587e86eb9d85074566042ad1f94a51f1ac5dd26
Neha-K-03 <nehak@neoito.com>
2025-08-23 15:31:54 +0530

    seconds passed

---
+++ PART 1: The JavaScript Language/Data types/5.11 Date and Time/seconds_passed.js
@@ -0,0 +1,13 @@
+// TASK 6
+
+function getSecondsToday() {
+    let now = new Date();
+
+    // create an object using the current day/month/year
+    let today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+
+    let diff = now - today;     // ms difference
+    return Math.round(diff / 1000);     // make seconds
+}
+
+alert( getSecondsToday() );

db27bfe369cbbc5a7354661ebfb481c1ffdf6b2b
Neha-K-03 <nehak@neoito.com>
2025-08-24 23:50:14 +0530

    format the relative date

---
+++ PART 1: The JavaScript Language/Data types/5.11 Date and Time/relative date.js
@@ -0,0 +1,41 @@
+// TASK 8
+
+function formatDate(date) { 
+    let diff = new Date() - date;     // difference in ms
+
+    if (diff < 1000) {     // less than 1 second, since 1 second = 1000 ms
+        return "right now";
+    }
+
+    let sec = Math.floor(diff / 1000);     // convert diff to seconds
+
+    if (sec < 60) {
+        return sec + " sec. ago";
+    }
+
+    let min = Math.floor(diff / 60000);     // convert diff to minutes
+    if (min < 60) {
+        return min + " min. ago";
+    }
+
+    // format the date
+    // add leading zeroes to single-digit day/month/hours/minutes
+    let d = date;
+    d = [
+        "0" + d.getDate(),
+        "0" + (d.getMonth()+1),
+        "" + d.getFullYear(),
+        "0" + d.getHours(),
+        "0" + d.getMinutes()
+    ].map(component => component.slice(-2));     // take last 2 digits of every component
+
+    // join the components into date
+    return d.slice(0, 3).join(".") + " " + d.slice(3).join(":");
+}
+
+alert( formatDate(new Date(new Date - 1)) );     // right now
+alert( formatDate(new Date(new Date - 30 * 1000)) );     // 30 sec. ago
+alert( formatDate(new Date(new Date - 5 * 60 * 1000)) );     // 5 min. ago
+
+// yesterdays's date like 31.12.2016 20:00
+alert( formatDate(new Date(new Date - 86400 * 1000)) );

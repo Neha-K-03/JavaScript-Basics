128c3293539cb8aeb620ae59b5004d1313024dff
Neha-K-03 <nehak@neoito.com>
2025-08-27 12:26:25 +0530

    function in if

---
+++ PART 1: The JavaScript Language/Advanced working with functions/6.3 Variable scope, closure/functionInIf.js
@@ -0,0 +1,19 @@
+// TASK 5
+
+let phrase = "Hello";
+
+if (true) {
+    let user = "John";
+
+    function sayHi() {
+        alert(`${phrase}, ${user}`);     // ReferenceError: sayHi is not defined
+    }
+}
+
+sayHi();
+
+
+// The result is an error.
+
+// The function "sayHi" is declared inside the "if",
+// so it only lives inside it. There is no "sayHi" outside.

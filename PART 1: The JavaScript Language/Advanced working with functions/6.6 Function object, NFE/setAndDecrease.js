88aea4a99471a13ba21cd8fc3ae6aa90f2042d36
Neha-K-03 <nehak@neoito.com>
2025-08-29 23:49:47 +0530

    set and decrease for counter

---
+++ PART 1: The JavaScript Language/Advanced working with functions/6.6 Function object, NFE/setAndDecrease.js
@@ -0,0 +1,15 @@
+// TASK 1
+
+function makeCounter() {
+    let count = 0;
+
+    function counter() {
+        return count++;
+    }
+
+    counter.set = value => count = value;
+
+    counter.decrease = () => count--;
+
+    return counter;
+}

726193c864c2f62d46a42e7e924fd69c0eb0c89c
Neha-K-03 <nehak@neoito.com>
2025-09-10 23:15:40 +0530

    add toString

---
+++ PART 1: The JavaScript Language/Prototypes, inheritance/8.4 Prototype methods, objects without __proto__/toString.js
@@ -0,0 +1,20 @@
+// TASK 1
+
+let dictionary = Object.create(null, {
+  toString: {     // define toString property
+    value() {     // the value is a function
+      return Object.keys(this).join();
+    }
+  }
+});
+
+dictionary.apple = "Apple";
+dictionary.__proto__ = "test";
+
+// apple and __proto__ is in the loop
+for(let key in dictionary) {
+  alert(key);     // "apple", then "__proto__"
+}
+
+// comma-separated list of properties by toString
+alert(dictionary);     // "apple,__proto__"

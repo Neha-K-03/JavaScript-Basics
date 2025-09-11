49dcb5497096e25447804884625dcab833dc7004
Neha-K-03 <nehak@neoito.com>
2025-09-11 13:39:42 +0530

    error creating an instance

---
+++ PART 1: The JavaScript Language/Classes/9.2 Class inheritance/error.js
@@ -0,0 +1,19 @@
+// TASK 1
+
+class Animal {
+
+  constructor(name) {
+    this.name = name;
+  }
+
+}
+
+class Rabbit extends Animal {
+  constructor(name) {
+    super(name);
+    this.created = Date.now();
+  }
+}
+
+let rabbit = new Rabbit("White Rabbit");     // ok now
+alert(rabbit.name);     // White Rabbit
